0550ca77a4ca34663b08127d1cce6b669665cf0d
EddieFreedome <eddie.freed@tutanota.com>
2021-10-11 17:31:30 +0200

    tried fixing js code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 //al click del menù a barre, sotto i 1000px, nascondere barre e mostrare menù hamburger (da display-none a display-block?)
-const hamburgerMenu = document.querySelector("hamburger-menu");
-const barsMenu = document.querySelector("close");
-const barsIcon = document.querySelector("fas fa-bars");
-const menuActive = document.querySelector("hamburger-menu-active")
+/*const hamburgerMenu = document.querySelector(".hamburger-menu");
+const barsMenu = document.querySelector(".close");
+const menuActive = document.querySelector(".hamburger-menu.active")*/
 
-//due azioni: 
+const barsIcon = document.querySelector(".header-right>.a");
+const hamburgerMenu = document.querySelector(".hamburger-menu")
+
+
+
 //1. al click su barsIcon, display none bars
 //2. al click sull'hamburger, display da none a block
 //3. al click sul .close, display none
-barsIcon addEventListener(click,
+barsIcon.addEventListener("click",
 
     function() {
+        //aggiungere classe active
+        hamburgerMenu.className = hamburgerMenu.classList + " active"
 
     }
+);
 //variabili: .hamburgerMenu bars-menu

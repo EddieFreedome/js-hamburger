1a7ff6794a0031d3675f843499b00b2330583759
EddieFreedome <eddie.freed@tutanota.com>
2021-10-11 18:11:42 +0200

    esercizio completato: inizializzato variabili e costruito funzioni chiave

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-//al click del menù a barre, sotto i 1000px, nascondere barre e mostrare menù hamburger (da display-none a display-block?)
-/*const hamburgerMenu = document.querySelector(".hamburger-menu");
-const barsMenu = document.querySelector(".close");
-const menuActive = document.querySelector(".hamburger-menu.active")*/
-
-const barsIcon = document.querySelector(".header-right>.a");
+const barsIcon = document.querySelector(".header-right>a");
 const hamburgerMenu = document.querySelector(".hamburger-menu")
+const closeIcon = document.querySelector(".close")
 
-
-
-//1. al click su barsIcon, display none bars
-//2. al click sull'hamburger, display da none a block
-//3. al click sul .close, display none
 barsIcon.addEventListener("click",
 
     function() {
         //aggiungere classe active
-        hamburgerMenu.className = hamburgerMenu.classList + " active"
-
+        //hamburgerMenu.className = hamburgerMenu.className + " active"
+        hamburgerMenu.classList.toggle("active")
     }
 );
-//variabili: .hamburgerMenu bars-menu
+
+closeIcon.addEventListener("click",
+
+    function() {
+        //togliere classe active
+        //hamburgerMenu.className = hamburgerMenu.className + " active"
+        hamburgerMenu.classList.toggle("active")
+    }
+);

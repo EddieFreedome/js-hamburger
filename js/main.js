5d5dd3b739f7538d6307dc5a89ed298b25bb69bb
EddieFreedome <eddie.freed@tutanota.com>
2021-10-11 17:06:54 +0200

    set excercise logic

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//al click del menù a barre, sotto i 1000px, nascondere barre e mostrare menù hamburger (da display-none a display-block?)
+const hamburgerMenu = document.querySelector("hamburger-menu");
+const barsMenu = document.querySelector("close");
+const barsIcon = document.querySelector("fas fa-bars");
+const menuActive = document.querySelector("hamburger-menu-active")
+
+//due azioni: 
+//1. al click su barsIcon, display none bars
+//2. al click sull'hamburger, display da none a block
+//3. al click sul .close, display none
+barsIcon addEventListener(click,
+
+    function() {
+
+    }
+//variabili: .hamburgerMenu bars-menu
